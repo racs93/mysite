c5dc8b5a5b80940f62beea67569ed3ca4a602f35
Rodrigo Correa <rodrigocorrea@localhost.localdomain>
2020-12-01 13:53:01 -0600

    part 6 tutorial

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,5 @@
+from django.utils import timezone
+
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
@@ -12,12 +14,16 @@ class IndexView(generic.ListView):
     template_name = 'polls/index.html'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte = timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
